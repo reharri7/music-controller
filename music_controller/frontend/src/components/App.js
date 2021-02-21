d544f41da036bb4a0730aed04ad8061377bac1bf
Rhett Harrison <reharri7@asu.edu>
2021-02-21 14:04:47 -0800

    Created API endpoints (create-room) to request a room to be created on the server. Also implemented React Router and connected 3 routes to the front end

--- music_controller/frontend/src/components/App.js
+++ music_controller/frontend/src/components/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
 
+import HomePage from './homepage';
+
+
 export default class App extends Component {
   constructor(props) {
     super(props);
@@ -9,7 +12,7 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>Hello World</h1>
+        <HomePage />
       </div>
     );
   }
